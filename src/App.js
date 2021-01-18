1e3bdf6427cd572706d96eef43a3b56790a9e249
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-01-18 18:58:40 -0300

    Add react-icons to project. Add icon to Task.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { useState } from 'react';
 import Header from './components/Header'
+import Tasks from './components/Tasks'
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      text: 'Some nonense',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 2,
+      text: 'Some nonense',
+      day: 'Feb 5th at 2:30pm',
+      reminder: true,
+    },
+    {
+      id: 3,
+      text: 'Some nonense',
+      day: 'Feb 5th at 2:30pm',
+      reminder: false,
+    }
+  ])
   return (
     <div className="container">
       <Header />
+      <Tasks tasks={tasks}/>
     </div>
   );
 }
