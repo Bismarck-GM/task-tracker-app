11ced71984f75e3fced6254157fff7e690af4b54
Gerónimo M <43770133+Bismarck-GM@users.noreply.github.com>
2021-01-19 12:07:40 -0300

    Add form and creation of tasks. Add state to form to display via header button.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,10 @@
 import Button from "./Button";
 
-const Header = ({title}) => {
-  const onClick = () => {
-    console.log('Click')
-  }
-
+const Header = ({ title, onAdd }) => {
   return (
     <header className='header'>
       <h1>{title}</h1>
-      <Button text='Hello' onClick={onClick}/>
+      <Button text='Hello' onClick={onAdd}/>
     </header>
   )
 }
